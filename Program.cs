92ab9da7d8fbfc4f208194e8769520f56323b754
Maslow5941 <maslow5941@hotmail.com>
2022-10-11 16:44:31 -0300

    criando meu primeiro dotnet OR

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool maiorIdade = true;
+
+bool autorizado = false;
+
+if(maiorIdade|| autorizado){
+  Console.WriteLine("autorizado");
+}
+else{
+  Console.WriteLine("entrada nao permitida");
+}
+
